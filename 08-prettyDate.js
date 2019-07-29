07ef244c89a57bc8b1ded4c8663d09e11469fc96
Sergio L. Benítez D <sergio.kun21@gmail.com>
2019-07-29 07:52:19 -0500

    UTJS: Push pending changes.

--- 08-prettyDate.js
+++ 08-prettyDate.js
@@ -1,13 +1,50 @@
 var prettyDate = {
-    format: function(now, time) {
+    calculateDateDifferences: function(now, time) {
         var date = new Date(time || "");
         var diff = (((new Date(now)).getTime() - date.getTime())/ 1000);
         var dayDiff = Math.floor(diff / 86400);
 
-    if (isNaN(dayDiff) || dayDiff < 0 || dayDiff >= 31) {
-        return;
-    }
+        if (isNaN(dayDiff) || dayDiff < 0 || dayDiff >= 31) {
+            return;
+        }
 
-    return formatDate(dayDiff, diff);
+        return formatDate(dayDiff, diff);
+    },
+
+    formatDate: function(dayDiff, diff) {
+        switch(true) {
+            case dayDiff === 0 && diff < 60:
+                return 'Just now';
+            case dayDiff === 0 && diff < 120:
+                return '1 minute ago';
+            case dayDiff === 0 && diff < 3600:
+                return Math.floor(diff / 60) + ' minutes ago';
+            case dayDiff === 0 && diff < 7200:
+                return '1 hour ago';
+            case dayDiff === 0 && diff < 86400:
+                return Math.floor(diff / 3600) + ' hours ago';
+            case dayDiff === 0 || dayDiff === 1:
+                return 'Yesterday';
+            case dayDiff === 0 || dayDiff < 7:
+                return dayDiff + ' days ago';
+            case dayDiff === 0 || dayDiff < 31:
+                return Math.ceil(dayDiff / 7) + ' days ago';
+            default:
+                return;
+        }
+    },
+
+    updateDate: function(now) {
+        var links = document.getElementsByTagName("a");
+
+        for(var i = 0; i < links.length; i++) {
+            if(links[i].title) {
+                var date = prettyDate.calculateDateDifferences(now, links[i].title)
+
+                if(date) {
+                    links[i].innerHTML = date;
+                }
+            }
+        }
     }
 }
